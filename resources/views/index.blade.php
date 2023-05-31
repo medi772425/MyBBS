f30dbafe9b7d0581486b95f5ad9dee320d7eb82b
medi772425 <medi772425@gmail.com>
2023-06-01 07:25:02 +0900

    一覧ページ作成

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>My BBS</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>My BBS</h1>
+        <ul>
+            @forelse ($posts as $index => $post)
+                <li>
+                    <a href="/posts/{{ $index }}">
+                        {{ $post }}
+                    </a>
+                </li>
+            @empty
+                <li>No posts yet!</li>
+            @endforelse
+        </ul>
+    </div>
+</body>
+</body>
+</html>
